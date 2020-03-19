5093ef6d16da6bb39f312c0393728577ce58fca4
nyan-soe <58061109+nyan-soe@users.noreply.github.com>
2020-03-19 09:15:30 -0300

    Fix errors

--- CodeInFigures/ErrorTest.java
+++ CodeInFigures/ErrorTest.java
@@ -1,6 +1,5 @@
-public class ErrorTest
-/*  This class displays a test message 
-{
+public class ErrorTest {
+/*  This class displays a test message */
    public static void main(String[] args)
    {
       System.out.println("Test");
